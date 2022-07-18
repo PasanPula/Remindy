6ca8bfca46bdc3f0ad24869c32d09b8a47301756
dasunsennath <dasunsennath123@gmail.com>
2022-07-18 13:27:57 +0530

    Complete MealReminder Intro Page

---
+++ app/src/main/java/com/usj/remindy/MealReminder_intro.java
@@ -0,0 +1,14 @@
+package com.usj.remindy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealReminder_intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_reminder_intro);
+    }
+}
