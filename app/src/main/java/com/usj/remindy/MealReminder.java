846bd475723a2781080e6f1400cfa62dc93af291
44DH1L <you@example.com>
2022-07-19 11:51:03 +0530

    Added link for all 5 activites

---
+++ app/src/main/java/com/usj/remindy/MealReminder.java
@@ -0,0 +1,14 @@
+package com.usj.remindy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MealReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal_reminder);
+    }
+}
