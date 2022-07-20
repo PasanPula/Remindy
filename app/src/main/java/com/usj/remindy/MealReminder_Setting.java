77fd8d12bcb8209aeb46e1300bef99b106c35031
dasunsennath <dasunsennath123@gmail.com>
2022-07-20 17:46:56 +0530

    Complete meal Setting page

--- app/src/main/java/com/usj/remindy/MealReminder_Setting.java
+++ app/src/main/java/com/usj/remindy/MealReminder_Setting.java
@@ -3,12 +3,36 @@ package com.usj.remindy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class MealReminder_Setting extends AppCompatActivity {
+
+
+    String[] items =  {"Breakfast","Lunch","Dinner"};
+    AutoCompleteTextView autoCompleteTxt;
+    ArrayAdapter<String> adapterItems;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_reminder);
+        autoCompleteTxt = findViewById(R.id.auto_complete_txt2);
+        adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,items);
+        autoCompleteTxt.setAdapter(adapterItems);
+
+        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
