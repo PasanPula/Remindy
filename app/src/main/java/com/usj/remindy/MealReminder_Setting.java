5b9ba9b70c92ffb79aeb21524ea572be98ad3bdb
dasunsennath <dasunsennath123@gmail.com>
2022-07-20 18:50:25 +0530

    Complete Back Button and links in meal reminder

--- app/src/main/java/com/usj/remindy/MealReminder_Setting.java
+++ app/src/main/java/com/usj/remindy/MealReminder_Setting.java
@@ -2,11 +2,13 @@ package com.usj.remindy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MealReminder_Setting extends AppCompatActivity {
@@ -25,11 +27,21 @@ public class MealReminder_Setting extends AppCompatActivity {
         adapterItems = new ArrayAdapter<String>(this,R.layout.list_item,items);
         autoCompleteTxt.setAdapter(adapterItems);
 
+        ImageButton Back =  findViewById(R.id.back);
+
         autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = parent.getItemAtPosition(position).toString();
                 Toast.makeText(getApplicationContext(),"Item: "+item,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MealReminder_Setting.this,MealReminder_intro.class);
+                startActivity(i);
             }
         });
 
--- app/src/main/java/com/usj/remindy/MealReminder_intro.java
+++ app/src/main/java/com/usj/remindy/MealReminder_intro.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MealReminder_intro extends AppCompatActivity {
 
@@ -15,14 +16,19 @@ public class MealReminder_intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_reminder_intro);
-        CardView cardBreakfast = findViewById(R.id.cardBreakfast);
-        cardBreakfast.setOnClickListener(new View.OnClickListener() {
+        ImageButton Back =  findViewById(R.id.back);
+        Back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MealReminder_intro.this,MealReminder_Setting.class);
+                Intent i = new Intent(MealReminder_intro.this,MainActivity.class);
                 startActivity(i);
             }
         });
     }
 
+
+    public void OpenSetting(View view) {
+        Intent i = new Intent(MealReminder_intro.this,MealReminder_Setting.class);
+        startActivity(i);
+    }
 }
