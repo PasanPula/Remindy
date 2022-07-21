0cc86b1c402a06597bb7142968fb34e05aa49f67
dasunsennath <dasunsennath123@gmail.com>
2022-07-21 14:31:48 +0530

    Add Notification To Reminder

--- app/src/main/java/com/usj/remindy/AddNewExpireDateReminder.java
+++ app/src/main/java/com/usj/remindy/AddNewExpireDateReminder.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AddNewExpireDateReminder extends AppCompatActivity {
 
@@ -13,7 +14,24 @@ public class AddNewExpireDateReminder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_expire_date_reminder);
+
+        ImageButton back = findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              btnCancle(v);
+            }
+        });
+
     }
+
+    public void btnCancle(View view) {
+        Intent inten = new Intent(AddNewExpireDateReminder.this,ExpiryDateReminder.class);
+       startActivity(inten);
+    }
+
+
 
 
 
