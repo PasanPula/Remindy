38fca67ba6045526bc000ab57de88449269f2b8d
PasanPula <ppulasithi@gmail.com>
2022-07-21 09:24:34 +0530

    link add new Ex reminder activity

--- app/src/main/java/com/usj/remindy/ExpiryDateReminder.java
+++ app/src/main/java/com/usj/remindy/ExpiryDateReminder.java
@@ -2,7 +2,10 @@ package com.usj.remindy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ExpiryDateReminder extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ExpiryDateReminder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expiry_date_reminder);
+
+        Button Addbtn=findViewById(R.id.addExReminder);
+        Addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ExpiryDateReminder.this,AddNewExpireDateReminder.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
